a53f174cbe677287e091aed0be13ad26030bff83
Shubham <shubhamsingh9816@gmail.com>
2023-10-28 18:41:56 +0530

    Final commit

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,18 +4,18 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAURcVbYUfibfV4J_oEvkxLHrM0c72BqTs",
-    authDomain: "typing-speed-website-993da.firebaseapp.com",
-    projectId: "typing-speed-website-993da",
-    storageBucket: "typing-speed-website-993da.appspot.com",
-    messagingSenderId: "420096175062",
-    appId: "1:420096175062:web:fc48066b199641b556d277",
-    measurementId: "G-19E5DC470Y"
-  };
+  apiKey: "AIzaSyBVILpywIBv1_-CrW7-XEvqx62mNBVCBsg",
+  authDomain: "typing-speed-test-b60e0.firebaseapp.com",
+  projectId: "typing-speed-test-b60e0",
+  storageBucket: "typing-speed-test-b60e0.appspot.com",
+  messagingSenderId: "106649979249",
+  appId: "1:106649979249:web:9a15dd68df2325cbed8ed0",
+  measurementId: "G-71R2CECC95"
+};
 
-  const firebaseapp =firebase.initializeApp(firebaseConfig)
+const firebaseapp = firebase.initializeApp(firebaseConfig)
 
-  const auth = firebase.auth();
-  const db = firebaseapp.firestore();
+const auth = firebase.auth();
+const db = firebaseapp.firestore();
 
-  export {auth,db} 
+export { auth, db } 
