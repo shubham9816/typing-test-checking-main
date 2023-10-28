f857efa3c101ccf05ba2a35676cb39d2f4512028
Shubham <shubhamsingh9816@gmail.com>
2023-10-28 18:28:44 +0530

    Final Commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { TestModeContextProvider } from "./Context/TestModeContext";
+import { ThemeContextProvider } from "./Context/ThemeContext";
+import { BrowserRouter } from "react-router-dom";
+import { Button, TextField } from '@mui/material';
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <ThemeContextProvider>
+      <TestModeContextProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </TestModeContextProvider>
+    </ThemeContextProvider>
+  </React.StrictMode>
+);
